176d6fbc309361cb8560bbc96f9efb69d6f9af76
Adrian Lineiro Arcos <adrlinei@c2r15s2.42madrid.com>
2025-02-07 20:32:34 +0100

    three buttons

--- scripts/example.gd
+++ scripts/example.gd
@@ -1,16 +1,38 @@
 extends Control
 
+var bebida_value: int = 100
+var comida_value: int = 100
+var juegos_value: int = 100  
 
-# Called when the node enters the scene tree for the first time.
+@onready var bebida_label = $Bebida_Label
+@onready var comida_label = $Comida_Label
+@onready var juegos_label = $Juegos_Label 
+
 func _ready():
-	pass # Replace with function body.
+	update_label()  
+
+func use_resource(resource: String, amount: int):
+	match resource:
+		"Bebida":
+			bebida_value = max(bebida_value - amount, 0)
+		"Comida":
+			comida_value = max(comida_value - amount, 0)
+		"Juegos":
+			juegos_value = max(juegos_value - amount, 0)
+	update_label()
+
+func update_label():
+	bebida_label.text = "Bebida: " + str(bebida_value) + "%"
+	comida_label.text = "Comida: " + str(comida_value) + "%"
+	juegos_label.text = "Juegos: " + str(juegos_value) + "%"
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _on_bebida_button_pressed():
+	use_resource("Bebida", 10) 
+
+func _on_comida_button_pressed():
+	use_resource("Comida", 10)
 
 
-func _on_button_button_down():
-	$Label.text = "alguna logica se hizo"
-	pass # Replace with function body.
+func _on_juegos_button_pressed():
+	use_resource("Juegos", 10)
